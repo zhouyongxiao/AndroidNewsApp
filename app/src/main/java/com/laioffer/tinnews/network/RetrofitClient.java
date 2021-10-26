70713efb4cb4f39dfcfc88f43adeb26e0362fc22
Yongxiao Zhou <jackzhouyongxiao@gmail.com>
2021-10-26 15:09:45 -0700

    Initial commit

---
+++ app/src/main/java/com/laioffer/tinnews/network/RetrofitClient.java
@@ -0,0 +1,42 @@
+package com.laioffer.tinnews.network;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    // TODO: Assign your API_KEY here
+    private static final String API_KEY = "51c1d6b38333499a8ae79555c31e6ee8";
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+
+    public static Retrofit newInstance() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HeaderInterceptor())
+                .addNetworkInterceptor(new StethoInterceptor()).build();
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    private static class HeaderInterceptor implements Interceptor {
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request original = chain.request();
+            Request request = original
+                    .newBuilder()
+                    .header("X-Api-Key", API_KEY)
+                    .build();
+            return chain.proceed(request);
+        }
+    }
+}
